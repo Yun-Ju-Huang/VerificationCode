9d0326d6de9fec7627423f8012fe14103380a760
Yun-Ju-Huang <jenny30307923@gmail.com>
2022-04-12 12:22:37 +0800

    templates + PY檔

---
+++ CAPTCHA.py
@@ -0,0 +1,18 @@
+# from crypt import methods
+# from curses import meta
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('home.html',name = name)
+
+
+
+if __name__ == '__main__':
+    app.run()
