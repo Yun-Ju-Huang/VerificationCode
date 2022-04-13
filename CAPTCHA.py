601e21315c1bb4a4af7127872c5975ae0fbbbf88
Yun-Ju-Huang <jenny30307923@gmail.com>
2022-04-13 16:52:58 +0800

    update .html

--- CAPTCHA.py
+++ CAPTCHA.py
@@ -1,20 +1,38 @@
-# from crypt import methods
-# from curses import meta
 from flask import Flask,render_template
+import requests
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+# API 的網域位址
+url = 'http://35.206.206.144:8000/photo'
 
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('home.html',name = name)
+# 需要解決的驗證碼圖片位址
+file_path = "D:\github\VerificationCode\TestImage\JNG5V.png"
+file_name = file_path.split('\\')[-1]
+file = open(file_path,'rb')
 
-@app.route('/home')
-def user():
-    return render_template('home.html')
+# file_name = "123.txt"
+# file = "123"
+files = {'file':(file_name, file, 'image/jpg/png')}
+r = requests.post(url, files = files)
 
-if __name__ == '__main__':
-    app.run()
+
+print(r.status_code)
+print(r.text)
+
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# # @app.route('/user/<name>')
+# # def user(name):
+# #     return render_template('home.html',name = name)
+
+# @app.route('/home')
+# def user():
+#     return render_template('home.html')
+
+# if __name__ == '__main__':
+#     app.run()
