d60f510da69b1bd8621e75fa662416b9b4502a21
Yun-Ju-Huang <jenny30307923@gmail.com>
2022-04-14 18:05:56 +0800

    更新HTML與程式碼

--- CAPTCHA.py
+++ CAPTCHA.py
@@ -1,36 +1,33 @@
-from flask import Flask,render_template
+from pickle import GET
+from flask import Flask,render_template,request,redirect,url_for
 import requests
 app = Flask(__name__)
 
-# API 的網域位址
+# # API 的網域位址
 url = 'http://35.206.206.144:8000/photo'
 
-# 需要解決的驗證碼圖片位址
-file_path = "D:\github\VerificationCode\TestImage\JNG5V.png"
-file_name = file_path.split('\\')[-1]
-file = open(file_path,'rb')
+@app.route('/')
+def home():    
+    return render_template('upload.html') 
 
-# file_name = "123.txt"
-# file = "123"
-files = {'file':(file_name, file, 'image/jpg/png')}
-r = requests.post(url, files = files)
-
-
-print(r.status_code)
-print(r.text)
-
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-# # @app.route('/user/<name>')
-# # def user(name):
-# #     return render_template('home.html',name = name)
-
-# @app.route('/home')
-# def user():
-#     return render_template('home.html')
-
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/return', methods=['GET', 'POST'])
+def retu():
+    
+    if request.method == 'GET':
+        return redirect(url_for("home"))
+    path = request.form['path']
+    if path == "":
+        return redirect(url_for("home"))
+    else:
+        file_name = path.split('\\')[-1]
+        file = open(path,'rb')
+        files = {'file':(file_name, file, 'image/jpg/png')}
+        asn = requests.post(url, files = files)
+        file = open(path,'rb')
+        print(path)
+        print(asn.status_code)
+        print(asn.text)
+        answer =asn.text
+        return render_template('return.html',answer=answer)
+if __name__ == '__main__':
+    app.run()
