99371053299eadc82c3601f1bf397e4ba0e29919
Yun-Ju-Huang <jenny30307923@gmail.com>
2022-04-15 15:22:01 +0800

    update

--- CAPTCHA.py
+++ CAPTCHA.py
@@ -1,7 +1,9 @@
-from pickle import GET
 from flask import Flask,render_template,request,redirect,url_for
 import requests
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 # # API 的網域位址
 url = 'http://35.206.206.144:8000/photo'
@@ -23,7 +25,6 @@ def retu():
         file = open(path,'rb')
         files = {'file':(file_name, file, 'image/jpg/png')}
         asn = requests.post(url, files = files)
-        file = open(path,'rb')
         print(path)
         print(asn.status_code)
         print(asn.text)
